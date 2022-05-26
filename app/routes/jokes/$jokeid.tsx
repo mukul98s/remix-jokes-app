45f6b96307406f96eb442fddeb29ecff110f9353
Mukul Sharma <mymukul112@gmail.com>
2022-05-26 22:55:21 +0530

    loaded data from the database

--- app/routes/jokes/$jokeid.tsx
+++ app/routes/jokes/$jokeid.tsx
@@ -1,11 +1,28 @@
+import type {LoaderFunction} from "@remix-run/node";
+import {json} from "@remix-run/node";
+import {Link, useLoaderData} from "@remix-run/react";
+import type {Joke} from "@prisma/client";
+
+import {db} from "~/utils/db.server";
+
+type LoaderData = {joke: Joke};
+
+export const loader: LoaderFunction = async ({params}) => {
+  const joke = await db.joke.findUnique({where: {id: params.jokeid}});
+
+  if (!joke) throw new Error("Joke not found");
+  const data: LoaderData = {joke};
+  return json(data);
+};
+
 export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
     </div>
   );
 }
--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,29 @@
+import type {LoaderFunction} from "@remix-run/node";
+import type {Joke} from "@prisma/client";
+import {useLoaderData} from "@remix-run/react";
+import {db} from "~/utils/db.server";
+
+type LoaderData = {randomJoke: Joke};
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomNumber = Math.floor(Math.random() * count);
+
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomNumber,
+  });
+
+  const data: LoaderData = {randomJoke};
+  return data;
+};
+
 export default function JokesIndexRoute() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div>
       <p>Here's a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>{data.randomJoke.content}</p>
     </div>
   );
 }
