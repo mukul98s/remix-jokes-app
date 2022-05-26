4f75c40c783071bc5ab654906afcd8dd6ce805c2
Mukul Sharma <mymukul112@gmail.com>
2022-05-27 00:01:28 +0530

    login page setup without functionality

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,9 +2,18 @@ import {PrismaClient} from "@prisma/client";
 const db = new PrismaClient();
 
 async function seed() {
+  const kody = await db.user.create({
+    data: {
+      username: "kody",
+      // this is a hashed version of "twixrox"
+      passwordHash:
+        "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
+    },
+  });
   await Promise.all(
     getJokes().map((joke) => {
-      return db.joke.create({data: joke});
+      const data = {jokesterId: kody.id, ...joke};
+      return db.joke.create({data});
     })
   );
 }
@@ -12,6 +21,8 @@ async function seed() {
 seed();
 
 function getJokes() {
+  // shout-out to https://icanhazdadjoke.com/
+
   return [
     {
       name: "Road worker",
